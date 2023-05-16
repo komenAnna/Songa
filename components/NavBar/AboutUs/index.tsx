a801e2693ce1d96dc05853318e0c1d78f7271212
Oscar Okumu <ookumu@songa.app>
2023-05-16 18:28:58 +0300

    added styling to the about us page

--- components/NavBar/AboutUs/index.tsx
+++ components/NavBar/AboutUs/index.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 
 const AboutUs: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">About Us</h1>
-      <p className="text-xl">
-        Who We Are; Boda industry is run by young Kenyans. At SONGA we seek to incooperate technology into the boda System.<br/>
-        10 towns and Counting|+1M registered rides| +20M Passengers
-      </p>
-      <h2 className="text-3xl font-bold mb-4">Mission</h2>
-      <p>By bringing together Key players in the boda boda industry, SONGA seeks to digitize boda boda transport<br/> 
-      through introducing technology and constatnt improvement</p><br/>
-
-      <h2 className="text-3xl font-bold mb-4">Values/ Our Beliefs</h2>
-      <p>Safety: Our riders are vetted, our bikes insured<br/>
-      Convinient: Our services are cheap and easy to help you plan<br/>
-      Customers Focus: Based on your interaction, we are able to come up with rides designed just for you
-      </p>
+    <div className="fixed bottom-4 left-4 bg-black w-96 h-80 p-4 shadow-lg flex items-start justify-start text-white font-montserrat font-semibold text-sm leading-6 overflow-auto">
+      <div>
+        <h1 className="text-xl font-bold mb-2">About Us</h1>
+        <p>
+          Who We Are; Boda industry is run by young Kenyans. At SONGA we seek to incorporate technology into the boda System.
+          10 towns and Counting | +1M registered rides | +20M Passengers
+        </p>
+        <h2 className="text-xl font-bold mb-2 mt-4">Mission</h2>
+        <p>
+          By bringing together key players in the boda boda industry, SONGA seeks to digitize boda boda transport
+          through introducing technology and constant improvement
+        </p>
+        <h2 className="text-xl font-bold mb-2 mt-4">Values/Our Beliefs</h2>
+        <p>
+          Safety: Our riders are vetted, our bikes insured
+          Convenient: Our services are cheap and easy to help you plan
+          Customer Focus: Based on your interaction, we are able to come up with rides designed just for you
+        </p>
+      </div>
     </div>
   );
 };
