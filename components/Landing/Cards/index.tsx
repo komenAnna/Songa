a1a530c04bd59a5bb405dbf5f5cb8b17aa67a972
komenAnna <komenriana@gmail.com>
2023-07-13 02:22:11 +0300

    [feat]:card responsiveness

--- components/Landing/Cards/index.tsx
+++ components/Landing/Cards/index.tsx
@@ -5,23 +5,23 @@ import { ArrowSmallRightIcon } from '@heroicons/react/24/solid'
 
 function Cards() {
   return (
-    <div className='w-full h-auto bg-white py-6 px-4  '>
+    <div className='w-screen h-auto bg-white py-6 px-4  '>
         {/* CARDS */}
         
-        <div className='mx-auto px-6 pt-64 md:pt-8 md:grid grid-cols-3 gap-8'>
-            <div className='w-auto bg-white shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300'>
-            <Image className='w-20 mx-auto mt-[-2rem] md:mt-2'
-                src='/assets/icons/card1-icon.png' alt='card-1 icon' width={84} height={84}
-            />
-            <h2 className='text-2xl text-green-600 font-bold text-center py-4 uppercase'>Safety</h2>
-            <p className='text-center text-black p-2 text-[14px]'>Our riders are vetted. Safety begins with you and everyone in  the society is tasked with their safety and those around them.</p>
-            {/* <Link href='/' className='text-red-500 text-center py-4 flex flex-row justify-center items-center gap-[0.5rem]'>
-                Learn More
-                <ArrowSmallRightIcon className='h-5 w-5 text-[#FB4552] ' /> 
-            </Link> */}
+        <div className='mx-auto px-6 md:pt-8 md:grid grid-cols-3 gap-8'>
+            <div className='w-[350px] mx-auto md:w-auto bg-white shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300'>
+                    <Image className='w-20 mx-auto mt-[-2rem] md:mt-2'
+                    src='/assets/icons/card1-icon.png' alt='card-1 icon' width={84} height={84}
+                    />
+                    <h2 className='text-2xl text-green-600 font-bold text-center py-4 uppercase'>Safety</h2>
+                    <p className='text-center text-black p-2 text-[14px]'>Our riders are vetted. Safety begins with you and everyone in  the society is tasked with their safety and those around them.</p>
+                    {/* <Link href='/' className='text-red-500 text-center py-4 flex flex-row justify-center items-center gap-[0.5rem]'>
+                        Learn More
+                        <ArrowSmallRightIcon className='h-5 w-5 text-[#FB4552] ' /> 
+                    </Link> */}  
         </div>
 
-        <div className='w-auto bg-white shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300'>
+        <div className='w-[350px] mx-auto md:w-auto bg-white shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300'>
             <Image className='w-20 mx-auto mt-[-2rem] md:mt-2'
                 src='/assets/icons/card2-icon.png' alt='card-1 icon' width={84} height={84}
             />
@@ -34,7 +34,7 @@ function Cards() {
             </Link> */}
         </div>
 
-        <div className='w-auto bg-white shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300'>
+        <div className='w-[350px] mx-auto md:w-auto bg-white shadow-xl flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300'>
             <Image className='w-20 mx-auto mt-[-2rem] md:mt-2'
                 src='/assets/icons/card3-icon.png' alt='card-1 icon' width={84} height={84}
             />
